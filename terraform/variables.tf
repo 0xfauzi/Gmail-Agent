51382d6c417a78ded59b1734dc0a7bc2f89a4771
0xfauzi <wumpini.hussein@gmail.com>
2024-07-16 00:03:02 +0100

    refine variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,7 @@
 variable "project_id" {
   description = "The ID of the Google Cloud project"
   type        = string
-}
-
-variable "secrets_project_id" {
-  description = "The ID of the secrets manager"
-  type = string
+  default     = "research-assistant-424819"
 }
 
 variable "region" {
@@ -14,17 +10,24 @@ variable "region" {
   default     = "europe-west2"
 }
 
+variable "image_tag" {
+  description = "The tag of the Docker image to deploy"
+  type        = string
+}
+
+variable "secrets_project_id" {
+  description = "The ID of the project containing secrets"
+  type        = string
+  default     = "99383323365"
+}
+
 variable "ai_agent_processor_image" {
-  description = "The Docker image for the AI Agent Processor"
+  description = "The full image name of the AI agent processor"
   type        = string
 }
 
 variable "watcher_service_account_id" {
-  description = "Service account ID for the gmail watcher"
-  type = string
-}
-
-variable "image_tag" {
-  description = "The tag of the AI Agent Processor image to deploy"
+  description = "The ID of the service account for the Gmail watcher"
   type        = string
+  default     = "service-99383323365"
 }
