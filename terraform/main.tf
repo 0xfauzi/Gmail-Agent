1e0858d207a84bd2b78bf73c390c26d46656b30b
0xfauzi <wumpini.hussein@gmail.com>
2024-07-16 22:18:06 +0100

    fix actions error

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,6 +25,11 @@ resource "google_project_service" "apis" {
   project = var.project_id
   service = each.key
   disable_on_destroy = false
+  disable_dependent_services = true
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
 }
 
 # Create Pub/Sub topics
