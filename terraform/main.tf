1d2f2c2cbd97a9ea2737eed050904cc9080f7818
0xfauzi <wumpini.hussein@gmail.com>
2024-07-23 23:28:03 +0100

    redeploy watcher schedule job

--- terraform/main.tf
+++ terraform/main.tf
@@ -404,6 +404,15 @@ resource "google_cloudfunctions2_function" "setup_watcher" {
   }
 }
 
+resource "google_cloudfunctions2_function_iam_member" "invoker" {
+  project        = google_cloudfunctions2_function.setup_watcher.project
+  location       = google_cloudfunctions2_function.setup_watcher.location
+  cloud_function = google_cloudfunctions2_function.setup_watcher.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "serviceAccount:${google_service_account.gmail_watcher.email}"
+}
+
 # Create a Cloud Scheduler job to trigger the watcher function
 resource "google_cloud_scheduler_job" "setup_watcher_job" {
   name             = "setup-gmail-watcher-job"
@@ -420,4 +429,13 @@ resource "google_cloud_scheduler_job" "setup_watcher_job" {
       service_account_email = google_service_account.gmail_watcher.email
     }
   }
+}
+
+resource "google_cloudfunctions2_function_iam_member" "scheduler_invoker" {
+  project        = google_cloudfunctions2_function.setup_watcher.project
+  location       = google_cloudfunctions2_function.setup_watcher.location
+  cloud_function = google_cloudfunctions2_function.setup_watcher.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "serviceAccount:${google_cloud_scheduler_job.setup_watcher_job.service_account_email}"
 }
