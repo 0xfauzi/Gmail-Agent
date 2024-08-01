b63ae4fb64043f7d88e9f1e0d0931f50e8aae3be
0xfauzi <wumpini.hussein@gmail.com>
2024-08-01 22:13:00 +0100

    we should be able to trigger email processing manually

--- src/gmail_watcher/main.py
+++ src/gmail_watcher/main.py
@@ -186,8 +186,17 @@ def fetch_changes(history_id, user_email):
 def pubsub_push(event, context):
     logger.info("Pub/Sub push received")
     try:
-        pubsub_message = base64.b64decode(event['data']).decode('utf-8')
-        data = json.loads(pubsub_message)
+        if isinstance(event, dict) and 'data' in event:
+            # This is a Pub/Sub event
+            pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+            data = json.loads(pubsub_message)
+        elif isinstance(event, dict):
+            # This is a direct invocation
+            data = event
+        else:
+            # This is an unexpected format
+            data = json.loads(event)
+            
         logger.info(f"Received Pub/Sub message data: {data}")
         
         user_email = data.get('emailAddress')
